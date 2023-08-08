e386a490961d0dc8ce3f2a8b14dafc4307b4c421
npapyan <npap@protonmail.com>
2023-08-07 22:37:28 -0400

    Clean up code

--- app/lib/component/Nutrition.tsx
+++ app/lib/component/Nutrition.tsx
@@ -1,14 +1,12 @@
 import NutrientRow from "./NutrientRow";
 
 export default function Nutrition({foodData}) {
-    let nutrientComponents = getNutrientComponents(foodData.foodNutrients);
     return (
         <div className="text-center pt-4">
             <h2 className="text-2xl">{foodData.brandName}</h2>
             <h3 className="text-xl">{foodData.productName}</h3>
             <h3 className="text-xl italic">{foodData.categories}</h3>
             <br />
-                {/* <div className="w-96 mx-auto flex-row text-center justify-center"> */}
                 <div>
                     <div className="w-96 mx-auto flex-row text-center justify-center">
                         <h1 className="text-2xl">Nutrition Facts</h1>
@@ -16,7 +14,7 @@ export default function Nutrition({foodData}) {
                     </div>
                     <hr />
                     <div>
-                        <div className="">
+                        <div>
                             <NutrientRow nutrient={{name: 'Total Fat', value: foodData.foodNutrients.fat_100g, unit: foodData.foodNutrients.fat_unit, isPrimary: true}}></NutrientRow>
                             <NutrientRow nutrient={{name: 'Cholesterol', value: foodData.foodNutrients.cholesteral_100g, unit: foodData.foodNutrients.cholesteral_unit, isPrimary: true}}></NutrientRow>
                             <NutrientRow nutrient={{name: 'Sodium', value: foodData.foodNutrients.sodium_100g, unit: foodData.foodNutrients.sodium_unit, isPrimary: true}}></NutrientRow>
@@ -27,9 +25,4 @@ export default function Nutrition({foodData}) {
                 </div>
         </div>
     )
-}
-
-function getNutrientComponents(foodNutrients: any) {
-
-
 }
